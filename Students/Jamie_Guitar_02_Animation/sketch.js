22cb2106afa76d5f8c4eb97244a62f304c2e9359
treefalldown <64744535+treefalldown@users.noreply.github.com>
2020-11-01 11:00:54 -0500

    Add files via upload

---
+++ Students/Jamie_Guitar_02_Animation/sketch.js
@@ -0,0 +1,696 @@
+  //Guitar Body
+  let circle01 = {
+  circleX:95,
+  circleY:348,
+  circleDiameter:110,
+  color:"black",
+  SpeedX: 3,
+  SpeedY: 4
+  }
+
+  let circle02 = {
+  circleX:110,
+  circleY:310,
+  circleDiameter:140,
+  color:"black",
+  SpeedX: 6,
+  SpeedY: 1
+  }
+
+  let circle03 = {
+  circleX:215,
+  circleY:333,
+  circleDiameter:105,
+  color:"black",
+  SpeedX: 1,
+  SpeedY: 5
+  }
+
+  let circle04 = {
+  circleX:244,
+  circleY:287,
+  circleDiameter:95,
+  color:"black",
+  SpeedX: 2,
+  SpeedY: 6
+  }
+
+  let circle05 = {
+  circleX:180,
+  circleY:429,
+  circleDiameter:100,
+  color:"teal",
+  SpeedX: 7,
+  SpeedY: 3
+  }
+
+  let circle06 = {
+  circleX:185,
+  circleY:204,
+  circleDiameter:110,
+  color:"teal",
+  SpeedX: 2,
+  SpeedY: 2
+  }
+
+  let circle07 = {
+  circleX:245,
+  circleY:335,
+  circleDiameter:60,
+  color:"teal",
+  SpeedX: 5,
+  SpeedY: 2
+  }
+
+  let circle08 = {
+  circleX:263,
+  circleY:283,
+  circleDiameter:45,
+  color:"teal",
+  SpeedX: 3,
+  SpeedY: 4
+  }
+
+  let circle09 = {
+  circleX:237,
+  circleY:372,
+  circleDiameter:16,
+  color:"black",
+  SpeedX: 11,
+  SpeedY: 6
+  }
+
+  let circle10 = {
+  circleX:268,
+  circleY:255,
+  circleDiameter:15,
+  color:"black",
+  SpeedX: 3,
+  SpeedY: 2
+  }
+
+  let circle11 = {
+  circleX:255,
+  circleY:285,
+  circleDiameter:30,
+  color:"teal",
+  SpeedX: 2,
+  SpeedY: 6
+  }
+
+  let circle12 = {
+  circleX:226,
+  circleY:336,
+  circleDiameter:21,
+  color:"teal",
+  SpeedX: 7,
+  SpeedY: 3
+  }
+
+  let rect01 = {
+  color: "black",
+  rectX: 135,
+  rectY: 245,
+  rectW: 77,
+  rectH: 100,
+  Speed: 4
+  }
+
+  let rect02 = {
+  color: "black",
+  rectX: 40,
+  rectY: 310,
+  rectW: 40,
+  rectH: 40,
+  Speed: 6
+  }
+
+  let rect03 = {
+  color: "teal",
+  rectX: 272,
+  rectY: 250,
+  rectW: 30,
+  rectH: 65,
+  Speed: 12
+  }
+
+  let rect04 = {
+  color: "teal",
+  rectX: 238,
+  rectY: 315,
+  rectW: 50,
+  rectH: 75,
+  Speed: 10
+  }
+
+  let rect05 = {
+  color: "black",
+  rectX: 240,
+  rectY: 285,
+  rectW: 15,
+  rectH: 15,
+  Speed: 8
+  }
+
+  let rect06 = {
+  color: "black",
+  rectX: 210,
+  rectY: 315,
+  rectW: 20,
+  rectH: 20,
+  Speed: 20
+  }
+
+  let quad01 = {
+  color: "black",
+  x1:10,
+  y1:161,
+  x2:95,
+  y2:140,
+  x3:90,
+  y3:60,
+  x4:5,
+  y4:70,
+  transX:100,
+  transY:240,
+  Angle: 10,
+  Speed: 1
+  }
+
+  let quad02 = {
+  color: "teal",
+  x1:-90,
+  y1:252,
+  x2:-32,
+  y2:230,
+  x3:-90,
+  y3:300,
+  x4:-40,
+  y4:260,
+  transX:200,
+  transY:150,
+  Angle: 10,
+  Speed: 2
+  }
+
+  let quad03 = {
+  color: "teal",
+  x1:-22,
+  y1:59,
+  x2:-80,
+  y2:40,
+  x3:-80,
+  y3:20,
+  x4:-10,
+  y4:40,
+  transX:200,
+  transY:200,
+  Angle: 10,
+  Speed: 10
+  }
+
+  let quad04 = {
+  color: "teal",
+  x1:-60,
+  y1:-85,
+  x2:-52,
+  y2:-90,
+  x3:-60,
+  y3:-70,
+  x4:-60,
+  y4:-80,
+  transX:320,
+  transY:350,
+  Angle: 10,
+  Speed: 3
+  }
+
+  // Guitar Neck
+  let quad05 = {
+  color: "black",
+  x1:-50,
+  y1:0,
+  x2:180,
+  y2:0,
+  x3:180,
+  y3:20,
+  x4:-180,
+  y4:28,
+  transX:300,
+  transY:300,
+  Angle: 10,
+  Speed: 5
+  }
+
+  // Head of Guitar
+  let circle13 = {
+  circleX:488,
+  circleY:301,
+  circleDiameter:10,
+  color:"black",
+  SpeedX: 4,
+  SpeedY: 6
+  }
+
+  let circle14 = {
+  circleX:565,
+  circleY:325,
+  circleDiameter:23,
+  color:"black",
+  SpeedX: 8,
+  SpeedY: 3
+  }
+
+  let circle15 = {
+  circleX:482,
+  circleY:340,
+  circleDiameter:40,
+  color:"teal",
+  SpeedX: 9,
+  SpeedY: 6
+  }
+
+  let circle16 = {
+  circleX:549,
+  circleY:340,
+  circleDiameter:20,
+  color:"teal",
+  SpeedX: 3,
+  SpeedY: 9
+  }
+
+  let rect07 = {
+  color: "black",
+  rectX: 480,
+  rectY: 295,
+  rectW: 80,
+  rectH: 25,
+  Speed: 13
+  }
+
+  let rect08 = {
+  color: "black",
+  rectX: 480,
+  rectY: 297,
+  rectW: 10,
+  rectH: 23,
+  Speed: 2
+  }
+
+  let rect09 = {
+  color: "black",
+  rectX: 480,
+  rectY: 320,
+  rectW: 75,
+  rectH: 18,
+  Speed: 17
+  }
+
+  let quad06 = {
+  color: "teal",
+  x1:230,
+  y1:80,
+  x2:234,
+  y2:77,
+  x3:234,
+  y3:60,
+  x4:230,
+  y4:60,
+  transX:250,
+  transY:220,
+  Angle: 10,
+  Speed: 9
+  }
+
+  let quad07 = {
+  color: "teal",
+  x1:221,
+  y1:-64,
+  x2:300,
+  y2:-45,
+  x3:300,
+  y3:-80,
+  x4:210,
+  y4:-80,
+  transX:270,
+  transY:360,
+  Angle: 10,
+  Speed: 15
+  }
+
+  let quad08 = {
+  color: "teal",
+  x1:248,
+  y1:40,
+  x2:180,
+  y2:50,
+  x3:248,
+  y3:110,
+  x4:180,
+  y4:110,
+  transX:300,
+  transY:290,
+  Angle: 10,
+  Speed: 4
+  }
+
+  //Tuning Keys
+  let quad09 = {
+  color: "black",
+  x1:153,
+  y1:-54,
+  x2:156,
+  y2:-52.5,
+  x3:157.5,
+  y3:-55.5,
+  x4:154.5,
+  y4:-57,
+  transX:340,
+  transY:350.5,
+  Angle: 10,
+  Speed: 5
+  }
+
+  let quad10 = {
+  color: "black",
+  x1:100,
+  y1:-106.5,
+  x2:99.5,
+  y2:-103.75,
+  x3:92.5,
+  y3:-106.25,
+  x4:94,
+  y4:-109,
+  transX:400,
+  transY:400,
+  Angle: 10,
+  Speed: 2
+  }
+
+  let quad11 = {
+  color: "black",
+  x1:93,
+  y1:96.5,
+  x2:96,
+  y2:98,
+  x3:97.5,
+  y3:95,
+  x4:94.5,
+  y4:93.5,
+  transX:413,
+  transY:203,
+  angle: 10,
+  speed: 3
+  }
+
+  let quad12 = {
+  color: "black",
+  x1:0,
+  y1:-126.5,
+  x2:-0.5,
+  y2:-123.75,
+  x3:-7.5,
+  y3:-126.25,
+  x4:-6,
+  y4:-129,
+  transX:513,
+  transY:423,
+  Angle: 10,
+  Speed: 1
+  }
+
+  let quad13 = {
+  color: "black",
+  x1:93,
+  y1:-3.5,
+  x2:96,
+  y2:-2,
+  x3:97.5,
+  y3:-5,
+  x4:94.5,
+  y4:-6.5,
+  transX:426,
+  transY:306,
+  Angle: 10,
+  Speed: 4
+  }
+
+  let quad14 = {
+  color: "black",
+  x1:300,
+  y1:93.5,
+  x2:299.5,
+  y2:96.25,
+  x3:292.5,
+  y3:93.75,
+  x4:294,
+  y4:91,
+  transX:226,
+  transY:206,
+  Angle: 10,
+  Speed: 2
+  }
+
+  let quad15 = {
+  color: "black",
+  x1:93,
+  y1:-3.5,
+  x2:96,
+  y2:-2,
+  x3:97.5,
+  y3:-5,
+  x4:94.5,
+  y4:-6.5,
+  transX:439,
+  transY:309,
+  Angle: 10,
+  Speed: 7
+  }
+
+  let quad16 = {
+  color: "black",
+  x1:300,
+  y1:93.5,
+  x2:299.5,
+  y2:96.25,
+  x3:292.5,
+  y3:93.75,
+  x4:294,
+  y4:91,
+  transX:239,
+  transY:209,
+  Angle: 10,
+  Speed: 12
+  }
+
+  let quad17 = {
+  color: "black",
+  x1:293,
+  y1:-103.5,
+  x2:296,
+  y2:-102,
+  x3:297.5,
+  y3:-105,
+  x4:294.5,
+  y4:-106.5,
+  transX:252,
+  transY:412,
+  Angle: 10,
+  Speed: 3
+  }
+
+  let quad18 = {
+  color: "black",
+  x1:100,
+  y1:193.5,
+  x2:99.5,
+  y2:196.25,
+  x3:92.5,
+  y3:193.75,
+  x4:94,
+  y4:191,
+  transX:452,
+  transY:112,
+  Angle: 10,
+  Speed: 1
+  }
+
+  let quad19 = {
+  color: "black",
+  x1:93,
+  y1:-203.5,
+  x2:96,
+  y2:-202,
+  x3:97.5,
+  y3:-205,
+  x4:94.5,
+  y4:-206.5,
+  transX:464,
+  transY:515,
+  Angle: 10,
+  Speed: 3
+  }
+
+  let quad20 = {
+  color: "black",
+  x1:0,
+  y1:-6.5,
+  x2:-0.5,
+  y2:-3.75,
+  x3:-7.5,
+  y3:-6.25,
+  x4:-6,
+  y4:-9,
+  transX:564,
+  transY:315,
+  Angle: 10,
+  Speed: 10
+  }
+
+  function setup() {
+  createCanvas(600,600)
+  }
+
+  function draw() {
+  background(0,128,128);
+  strokeWeight(0)
+
+  //Guitar Body
+  //Black Shapes
+  drawCircle(circle01)
+
+  drawCircle(circle02)
+
+  drawCircle(circle03)
+
+  drawQuad(quad01)
+
+  drawRect(rect01)
+
+  drawRect(rect02)
+
+  drawCircle(circle04)
+
+  //Background Color Shapes
+  drawCircle(circle05)
+
+  drawCircle(circle06)
+
+  drawQuad(quad02)
+
+  drawQuad(quad03)
+
+  drawRect(rect03)
+
+  drawRect(rect04)
+
+  drawCircle(circle07)
+
+  drawCircle(circle08)
+
+  //Overlaid Black Shapes
+  drawCircle(circle09)
+
+  drawCircle(circle10)
+
+  drawRect(rect05)
+
+  drawRect(rect06)
+
+  //Overlaid Background Color Shapes
+  drawQuad(quad04)
+
+  drawCircle(circle11)
+
+  drawCircle(circle12)
+
+  //Guitar Neck
+  drawQuad(quad05)
+
+  //Head of Guitar
+  //Black Shapes
+  drawRect(rect07)
+
+  drawRect(rect08)
+
+  drawCircle(circle13)
+
+  drawCircle(circle14)
+
+  drawRect(rect09)
+
+  //Background Color Shapes
+  drawQuad(quad06)
+
+  drawQuad(quad07)
+
+  drawCircle(circle15)
+
+  drawCircle(circle16)
+
+  drawQuad(quad08)
+
+  //Tuning keys
+  drawQuad(quad09)
+
+  drawQuad(quad10)
+
+  drawQuad(quad11)
+
+  drawQuad(quad12)
+
+  drawQuad(quad13)
+
+  drawQuad(quad14)
+
+  drawQuad(quad15)
+
+  drawQuad(quad16)
+
+  drawQuad(quad17)
+
+  drawQuad(quad18)
+
+  drawQuad(quad19)
+
+  drawQuad(quad20)
+  }
+
+  function drawCircle(incomingCircle) {
+  fill(incomingCircle.color)
+  push();
+  if(incomingCircle.circleX < 0 || incomingCircle.circleX > width) {
+  incomingCircle.SpeedX = incomingCircle.SpeedX *-1;
+  }
+  incomingCircle.circleX = incomingCircle.circleX + incomingCircle.SpeedX;
+  if(incomingCircle.circleY < 0 || incomingCircle.circleY > height) {
+  incomingCircle.SpeedY = incomingCircle.SpeedY *-1;
+  }
+  incomingCircle.circleY = incomingCircle.circleY + incomingCircle.SpeedY;
+  translate(incomingCircle.circleX,incomingCircle.circleY)
+  circle(0,0,incomingCircle.circleDiameter)
+  pop();
+  }
+
+  function drawRect(incomingRect) {
+  fill(incomingRect.color);
+  push();
+  translate(incomingRect.rectX,incomingRect.rectY);
+  incomingRect.rectW = incomingRect.rectW + incomingRect.Speed/10;
+  incomingRect.rectH = incomingRect.rectH + incomingRect.Speed/10;
+  if(incomingRect.rectW < 10 || incomingRect.rectW > 100) {
+  incomingRect.Speed = incomingRect.Speed * -1;
+  }
+  rect(0,0,incomingRect.rectW, incomingRect.rectH);
+  pop();
+  }
+
+  function drawQuad(incomingQuad){
+  fill(incomingQuad.color)
+  push();
+  translate(incomingQuad.transX,incomingQuad.transY)
+  rotate(incomingQuad.Angle);
+  incomingQuad.Angle = incomingQuad.Angle + incomingQuad.Speed* 0.01;
+  quad(incomingQuad.x1,incomingQuad.y1,incomingQuad.x2,incomingQuad.y2,incomingQuad.x3,incomingQuad.y3,incomingQuad.x4,incomingQuad.y4);
+  pop();
+  }
